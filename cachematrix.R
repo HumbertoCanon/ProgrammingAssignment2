7cbd8d2045534c36fcf6024a576a1df4966cbeeb
HumbertoCanon <hcanonme@gmail.com>
2020-07-10 19:27:33 -0500

    Changes done to create: 'maCacheMatrix' and "cacheSolve" as required in order to create and store Inverse Matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,53 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
-}
+##                     Calculate Inverse matrix
 
 
-## Write a short comment describing this function
+##In this Programming Assignment we will take advantage of the scoping rules of
+##the R language and how they can be manipulated to preserve state inside
+##of an R object.
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+
+##Matrix inversion is usually a costly computation and there may be some
+##benefit to caching the inverse of a matrix rather than computing it
+##repeatedly
+
+
+##  Two Function will be created:
+##
+##  1.  `makeCacheMatrix`: This function creates a special "matrix" object
+##  that can cache its inverse.
+
+makeCacheMatrix <- function(x = matrix())
+    {
+      inv <- NULL
+      set <- function(y) 
+        {
+          x <<- y
+          inv <<- NULL
+        }
+      get <- function() x
+      setInvrs <- function(invrs) inv <<- invrs
+      getInvrs <- function() inv
+      list(set = set, get = get, setInvrs = setInvrs, getInvrs = getInvrs)
+    }
+
+##  2.  `cacheSolve`: This function computes the inverse of the special
+##  "matrix" returned by `makeCacheMatrix` above. If the inverse has
+##  already been calculated (and the matrix has not changed), then
+##  `cacheSolve` should retrieve the inverse from the cache.
+
+cacheSolve <- function(x, ...) 
+    {
+      inv <- x$getInvrs()
+      if (!is.null(inv))
+        {
+          message("getting cached data")
+          return(inv)
+        }
+      mat <- x$get()
+      inv <- solve(mat, ...)
+      x$setInvrs(inv)
+      inv
+    }
+
+##Most of the code was done using an example delivered in the beginning of
+##the assignation
